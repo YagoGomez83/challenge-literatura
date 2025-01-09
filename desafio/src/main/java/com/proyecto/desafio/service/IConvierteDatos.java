fa6c07a0447ddfbeac3382084fe75329c5c80231
Yago Gomez <104085125+YagoGomez83@users.noreply.github.com>
2025-01-08 22:35:29 -0300

    Remove incorrect files

--- desafio/src/main/java/com/proyecto/desafio/service/IConvierteDatos.java
+++
@@ -1,6 +0,0 @@
-package com.proyecto.desafio.service;
-
-public interface IConvierteDatos {
-    // Método genérico para convertir un JSON en una clase específica
-    <T> T obtenerDatos(String json, Class<T> clase);
-}
